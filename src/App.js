2f4937bbb34264173d98591f14656d7d79aca4d8
darkpsinight <darkpsinight@hotmail.com>
2022-04-30 18:13:31 +0100

    30/4/2022

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import { db } from './firebase-config';
+import { collection } from "firebase/firestore";
 
 function App() {
+
+  const [users, setusers] = useState([])
+  const usersCollectionRef = collection(db, "users")
+
+  useEffect(() => {
+    const getUsers = async () => {
+
+    }
+    getUsers()
+  }, [])
+
+
+
+
+
+
+
+
   return (
     <div className="App">
-      
+
     </div>
   );
 }
