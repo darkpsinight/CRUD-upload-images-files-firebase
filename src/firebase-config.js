893747bd9d0423969cb7d700283c7c1566b680c4
darkpsinight <darkpsinight@hotmail.com>
2022-04-30 16:16:37 +0100

    30/4/2022

---
+++ src/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCtPUMZF1pfQCTtrBkuaglZaGTO9bZxNEg",
+    authDomain: "crud-firebase-8892b.firebaseapp.com",
+    projectId: "crud-firebase-8892b",
+    storageBucket: "crud-firebase-8892b.appspot.com",
+    messagingSenderId: "1012123760642",
+    appId: "1:1012123760642:web:cb7dc8eaf0756874a0cf1b",
+    measurementId: "G-PQCDFZXTYD"
+};0
+
+const app = initializeApp(firebaseConfig);
